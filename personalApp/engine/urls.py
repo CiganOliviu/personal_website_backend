55f00cbdeab1bbfc586c9e5542ee61e5c1ddf859
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-07-06 20:36:21 +0300

    Update backend

--- personalApp/engine/urls.py
+++ personalApp/engine/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.index),
+  path('books/', views.books),
 ]
--- personalApp/engine/views.py
+++ personalApp/engine/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index (request):
     return render (request, "main/index.html")
+
+def books (request):
+    return render (request, "main/books.html")
