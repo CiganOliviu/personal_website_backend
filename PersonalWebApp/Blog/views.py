503e3f8e9868bd5828a2f82725f14b22bb9680d9
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-09-04 21:29:01 +0300

    Update personal-web-app backend

--- PersonalWebApp/Blog/views.py
+++ PersonalWebApp/Blog/views.py
@@ -5,6 +5,7 @@ from .models import Post
 class PostList(generic.ListView):
 
     queryset = Post.objects.filter(status=1).order_by('-created_on')
+    
     template_name = 'blog/blog.html'
 
 class PostDetail(generic.DetailView):
